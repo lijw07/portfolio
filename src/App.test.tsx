9872b77127347d87d27649ca70bbddabea2e03ee
Jai Li <tommyli874@gmail.com>
2025-07-30 17:15:07 -0400

    Added simple portfloio

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders portfolio app', () => {
-  render(<App />);
-  const titleElement = screen.getByText(/Jai Li/i);
-  expect(titleElement).toBeInTheDocument();
-});
